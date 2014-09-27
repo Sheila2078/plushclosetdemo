520bcb935f74949a070d075e3ed4832416784dfc
Sheila Waples <sheilawaples@aol.com>
2014-09-27 00:52:23 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Plushclosetdemo::Application.config.secret_key_base = '02bc2204f3d18d82f531bc2c6588bdfe993b95c1306dfceee3c2ede9c07b8e043004d83684b9bbb3b64e7c96077e54b1a62f70b4335e71c2a0c3049639d96dbd'
